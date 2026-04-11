193e830ce96313dfbc503a380e9eb028320ae5a4
Zabedfolio <zabedfolio@gmail.com>
2026-04-11 14:32:30 +0600

    next js

---
+++ my-next-app/src/app/about/developers/page.jsx
@@ -0,0 +1,10 @@
+
+const DevelopersPage = () => {
+    return (
+        <div>
+            <h2>this is developers page</h2>
+        </div>
+    );
+};
+
+export default DevelopersPage;
---
+++ my-next-app/src/app/contact/page.jsx
@@ -0,0 +1,10 @@
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h2 className='text-center text-6xl'>Contact page</h2>
+        </div>
+    );
+};
+
+export default ContactPage;
